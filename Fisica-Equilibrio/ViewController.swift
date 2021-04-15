847c690fb10defeb15679732f592c2bd8b691db7
Ricardo Sergio Gómez Cárdenas <A01235366@itesm.mx>
2021-04-15 08:26:53 -0700

    Initial Commit

---
+++ Fisica-Equilibrio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fisica-Equilibrio
+//
+//  Created by user190336 on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

4c58824342d78305804196f61148d8dccad663b9
user189096 <user189096@74-80-239-61.ic2net.net>
2021-05-14 10:16:39 -0700

    Fix bugs: Bricks are now vertically placed correctly, brick positions can be used more than once, bricks are visible when moving, bricks now animate to new position

--- Fisica-Equilibrio/SimulatorScene.swift
+++ Fisica-Equilibrio/SimulatorScene.swift
@@ -20,13 +20,14 @@ class SimulatorScene: SKScene {
     let scaleBase = SKSpriteNode(imageNamed: "base")
     let floor = SKSpriteNode()
     var bricks = [BrickNode]()
-    var selectedNode = SKSpriteNode()
+    var selectedBrick = BrickNode()
     
     let positionBrick = SKShapeNode(rectOf: CGSize(width: 120, height: 50))
     var xScalePositions = [CGFloat]()
     var brickPosition : Int = 0
     var xBrickPosition : CGFloat = 0
     var occupiedPositions = [false, false, false, false, false, false, false, false]
+    var brickIsSelected : Bool = false
     
     override func didMove(to view: SKView) {
         background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
@@ -67,10 +68,25 @@ class SimulatorScene: SKScene {
     
     func addBrick(brickWeight: Int) {
         let brick = BrickNode(imageNamed: String(brickWeight))
-        brick.position = CGPoint(x: self.frame.width * 0.5, y: self.frame.height * 0.7)
+        brick.position = CGPoint(x: 0, y: scale.size.height * 8)
         brick.setup(brickWeight: brickWeight)
-        addChild(brick)
+        scale.addChild(brick)
         bricks.append(brick)
+    }
+    
+    func selectBrick(location: CGPoint) {
+        let touchedNode = self.atPoint(location)
+        
+        if touchedNode is SKSpriteNode && touchedNode.name == "brick" {
+            if !selectedBrick.isEqual(touchedNode) {
+                selectedBrick = touchedNode as! BrickNode
+            }
+            
+            positionBrick.position = CGPoint(x: scale.size.width, y: scale.size.height)
+            scale.addChild(positionBrick)
+            
+            brickIsSelected = true
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -78,77 +94,69 @@ class SimulatorScene: SKScene {
         let location = touch.location(in: self)
         
         selectBrick(location: location)
-        
-        positionBrick.position = CGPoint(x: scale.size.width * 0.0625, y: scale.size.height)
-        scale.addChild(positionBrick)
+        if selectedBrick.brickPosition != -1 {
+            occupiedPositions[selectedBrick.brickPosition] = false
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            selectedNode.position.x = location.x
-            selectedNode.position.y = location.y
-            
-            if (location.x < xScalePositions[0]) {
-                brickPosition = 0
-                xBrickPosition = -0.4375
+        if brickIsSelected {
+            for touch in touches {
+                let location = touch.location(in: self)
+                selectedBrick.position.x = location.x - scale.position.x
+                selectedBrick.position.y = location.y - scale.position.y
+                
+                if (location.x < xScalePositions[0]) {
+                    brickPosition = 0
+                    xBrickPosition = -0.4375
+                }
+                else if (location.x < xScalePositions[1]) {
+                    brickPosition = 1
+                    xBrickPosition = -0.3125
+                }
+                else if (location.x < xScalePositions[2]) {
+                    brickPosition = 2
+                    xBrickPosition = -0.1875
+                }
+                else if (location.x < xScalePositions[3]) {
+                    brickPosition = 3
+                    xBrickPosition = -0.0625
+                }
+                else if (location.x < xScalePositions[4]) {
+                    brickPosition = 4
+                    xBrickPosition = 0.0625
+                }
+                else if (location.x < xScalePositions[5]) {
+                    brickPosition = 5
+                    xBrickPosition = 0.1875
+                }
+                else if (location.x < xScalePositions[6]) {
+                    brickPosition = 6
+                    xBrickPosition = 0.3125
+                }
+                else {
+                    brickPosition = 7
+                    xBrickPosition = 0.4375
+                }
+                positionBrick.position.x = scale.size.width * xBrickPosition
             }
-            else if (location.x < xScalePositions[1]) {
-                brickPosition = 1
-                xBrickPosition = -0.3125
-            }
-            else if (location.x < xScalePositions[2]) {
-                brickPosition = 2
-                xBrickPosition = -0.1875
-            }
-            else if (location.x < xScalePositions[3]) {
-                brickPosition = 3
-                xBrickPosition = -0.0625
-            }
-            else if (location.x < xScalePositions[4]) {
-                brickPosition = 4
-                xBrickPosition = 0.0625
-            }
-            else if (location.x < xScalePositions[5]) {
-                brickPosition = 5
-                xBrickPosition = 0.1875
-            }
-            else if (location.x < xScalePositions[6]) {
-                brickPosition = 6
-                xBrickPosition = 0.3125
-            }
-            else {
-                brickPosition = 7
-                xBrickPosition = 0.4375
-            }
-            positionBrick.position.x = scale.size.width * xBrickPosition
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        positionBrick.removeFromParent()
-        
-        if !occupiedPositions[brickPosition] {
-            occupiedPositions[brickPosition] = true
-            selectedNode.removeFromParent()
-            print([scale.size.width, xBrickPosition, scale.size.width * xBrickPosition, scale.size.height])
-            selectedNode.position = CGPoint(x: scale.size.width * xBrickPosition, y: scale.size.height)
-            print([selectedNode.position.x, selectedNode.position.y])
-            //selectedNode.run(SKAction.move(to: CGPoint(x: scale.size.width * xBrickPosition, y: scale.size.height), duration: 5))
-            scale.addChild(selectedNode)
-        }
-        else {
-            selectedNode.removeFromParent()
-        }
-    }
-    
-    func selectBrick(location: CGPoint) {
-        let touchedNode = self.atPoint(location)
-        
-        if touchedNode is SKSpriteNode && touchedNode.name == "brick" {
-            if !selectedNode.isEqual(touchedNode) {
-                selectedNode = touchedNode as! SKSpriteNode
+        if brickIsSelected {
+            positionBrick.removeFromParent()
+            
+            if !occupiedPositions[brickPosition] {
+                occupiedPositions[brickPosition] = true
+                selectedBrick.brickPosition = brickPosition
+                selectedBrick.run(SKAction.move(to: CGPoint(x: scale.size.width * xBrickPosition, y: (scale.size.height / 2) + (selectedBrick.size.height / 2)), duration: 0.15))
             }
+            else {
+                selectedBrick.removeFromParent()
+            }
+            
+            brickIsSelected = false
         }
     }
     
